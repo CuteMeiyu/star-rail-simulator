948f3d683bdea409e635072f07b80e26782249fd
CuteMeiyu <151729943+CuteMeiyu@users.noreply.github.com>
2025-05-14 22:32:04 +0800

    relic base class

--- hsr/relics/relic.py
+++ hsr/relics/relic.py
@@ -1,5 +1,16 @@
 import game
 
 
-class Relic(game.SourcelessMod):
-    pass
+class Relic(game.Mod):
+    def __init__(self, name: str, unit: game.Unit, stats: game.Stats) -> None:
+        self.name = name
+        self.stats = stats
+        super().__init__(unit, unit)
+
+    def add(self):
+        self.unit.stats += self.stats
+        return super().add()
+
+    def remove(self):
+        self.unit.stats -= self.stats
+        return super().remove()
